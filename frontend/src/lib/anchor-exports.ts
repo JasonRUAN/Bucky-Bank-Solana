679a2af163ca806da6120d29318d32a4ae1e9f66
JasonRUAN <rzexin@gmail.com>
2025-10-26 10:15:36 +0800

    代码格式化

--- frontend/src/lib/anchor-exports.ts
+++ frontend/src/lib/anchor-exports.ts
@@ -12,27 +12,29 @@ export const bucky_bank_PROGRAM_ID = new PublicKey(BuckyBankIDL.address);
 
 // This is a helper function to get the Counter Anchor program.
 export function getBuckyBankProgram(
-  provider: AnchorProvider,
-  address?: PublicKey
+    provider: AnchorProvider,
+    address?: PublicKey
 ): Program<BuckyBank> {
-  return new Program(
-    {
-      ...BuckyBankIDL,
-      address: address ? address.toBase58() : BuckyBankIDL.address,
-    } as BuckyBank,
-    provider
-  );
+    return new Program(
+        {
+            ...BuckyBankIDL,
+            address: address ? address.toBase58() : BuckyBankIDL.address,
+        } as BuckyBank,
+        provider
+    );
 }
 
 // This is a helper function to get the program ID for the Counter program depending on the cluster.
 export function getBuckyBankProgramId(cluster: Cluster) {
-  switch (cluster) {
-    case "devnet":
-    case "testnet":
-      // This is the program ID for the Counter program on devnet and testnet.
-      return new PublicKey("2N4emW88bMtPELSz6s62oTt48enm2yo8KbSdk9BeGPBG");
-    case "mainnet-beta":
-    default:
-      return bucky_bank_PROGRAM_ID;
-  }
+    switch (cluster) {
+        case "devnet":
+        case "testnet":
+            // This is the program ID for the Counter program on devnet and testnet.
+            return new PublicKey(
+                "2N4emW88bMtPELSz6s62oTt48enm2yo8KbSdk9BeGPBG"
+            );
+        case "mainnet-beta":
+        default:
+            return bucky_bank_PROGRAM_ID;
+    }
 }
